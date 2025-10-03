84b57448b44de8205e167ff35b2c33aba872161a
Tajinder <tnijjar2@my.centennialcollege.ca>
2025-10-03 11:34:07 -0400

    Add Separate Movement For Second Paddle

--- Pong Copy/Assets/Scripts/PaddleMovement.cs
+++ Pong Copy/Assets/Scripts/PaddleMovement.cs
@@ -5,30 +5,50 @@ public class PaddleMovement : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed = 4f;
-    private InputAction m_moveAction;
+    private InputAction m_moveAction_player1;
+    private InputAction m_moveAction_player2;
     private Vector2 moveInput;
+    private Vector2 moveInput2;
+    private GameObject player;
     Rigidbody2D rb;
 
     private void Awake()
     {
-        m_moveAction = InputSystem.actions.FindAction("Move");
+        m_moveAction_player1 = InputSystem.actions.actionMaps[0].FindAction("Move");
+        m_moveAction_player2 = InputSystem.actions.actionMaps[1].FindAction("Move");
+        player = this.gameObject;
         rb = GetComponent<Rigidbody2D>();
 
     }
 
     private void OnEnable()
     {
-        m_moveAction.Enable();
+        m_moveAction_player1.Enable();
+        m_moveAction_player2.Enable();
     }
 
     private void OnDisable()
     {
-        m_moveAction.Disable();
+        m_moveAction_player1.Disable();
+        m_moveAction_player2.Disable();
     }
 
     private void FixedUpdate()
     {
-        moveInput = m_moveAction.ReadValue<Vector2>();
-        rb.linearVelocityY = moveInput.y * moveSpeed;
+        //player1
+        if(player.tag.Equals("Player1"))
+        {
+            Debug.Log("tag correct");
+            moveInput = m_moveAction_player1.ReadValue<Vector2>();
+            rb.linearVelocityY = moveInput.y * moveSpeed;
+        }
+
+        //player2
+        if (player.tag.Equals("Player2"))
+        {
+            Debug.Log("tag correct");
+            moveInput = m_moveAction_player2.ReadValue<Vector2>();
+            rb.linearVelocityY = moveInput.y * moveSpeed;
+        }
     }
 }
